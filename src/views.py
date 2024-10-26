e6c2cbff425dba54ff6e06bcc9d33de6314a578e
Wadson Garbes <garbesdev@gmail.com>
2024-10-25 21:26:03 -0300

    Adiciona templates

--- src/views.py
+++ src/views.py
@@ -1,6 +1,8 @@
 from flask import current_app as app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username': 'Neno'}
+    return render_template('index.html', title='Home', user=user)
